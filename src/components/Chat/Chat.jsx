aee8d293f9df62c0fa0cf8c132bb9c55e7b8cc67
Zack Wilson <zackazt@gmail.com>
2020-12-15 09:39:57 -0500

    Chat Engine

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,1 +1,44 @@
-export const Chat = () => <>I am the Chat</>;
+import { useChat } from 'context/ChatContext';
+import { useEffect } from 'react';
+import { getChats, ChatEngine } from 'react-chat-engine';
+
+export const Chat = () => {
+  const { myChats, setMyChats, chatConfig, selectedChat } = useChat();
+
+  useEffect(() => {
+    console.log('My Chats: ', myChats);
+  }, [myChats]);
+
+  return (
+    <>
+      {!!chatConfig && (
+        <ChatEngine
+          hideUI={true}
+          userName={chatConfig.userName}
+          projectID={chatConfig.projectID}
+          userSecret={chatConfig.userSecret}
+          onConnect={() => {
+            getChats(chatConfig, setMyChats);
+          }}
+        />
+      )}
+
+      <div className="chat-container">
+        <div className="current-chat">
+          {selectedChat ? (
+            <></>
+          ) : (
+            <div className="no-chat-selected">
+              <img
+                src="/img/pointLeft.png"
+                className="point-left"
+                alt="point-left"
+              />
+              Select A Chat
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
