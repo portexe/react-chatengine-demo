59c0d52aea20c820ce39d4ef2321b1bb1efd6eff
Zack Wilson <zackazt@gmail.com>
2020-12-15 13:29:27 -0500

    Remove unnecessary if statement

--- src/components/ChatAvatar/ChatAvatar.jsx
+++ src/components/ChatAvatar/ChatAvatar.jsx
@@ -1,25 +1,23 @@
 import { fb } from 'service';
 import { useChat } from 'context';
 import { Image } from 'semantic-ui-react';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const ChatAvatar = ({ chat, username, className }) => {
   const { chatConfig } = useChat();
   const [avatar, setAvatar] = useState('');
 
   useEffect(() => {
-    if (chat.people.length === 2) {
-      fb.firestore
-        .collection('chatUsers')
-        .where('userName', '==', username)
-        .get()
-        .then(snap => {
-          const data = snap.docs[0]?.data();
-          if (data?.avatar) {
-            setAvatar(data.avatar);
-          }
-        });
-    }
+    fb.firestore
+      .collection('chatUsers')
+      .where('userName', '==', username)
+      .get()
+      .then(snap => {
+        const data = snap.docs[0]?.data();
+        if (data?.avatar) {
+          setAvatar(data.avatar);
+        }
+      });
   }, [chat, chatConfig, username]);
 
   return avatar ? (
